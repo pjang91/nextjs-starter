47e5ad4813d521575edaa5ca20d513ea1f1585d0
Paul Jang <pjang91@gmail.com>
2023-09-28 07:31:50 -0400

    next js crashed

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -22,10 +22,24 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
 }
